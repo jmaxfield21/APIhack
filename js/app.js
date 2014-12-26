04b2ff010de41f5218d3d38c02de7ae08a6800a4
jmaxfield21 <Jace@Maxfields-iMac.local>
2014-12-26 15:01:47 -0700

    google maps api functioning

--- js/app.js
+++ js/app.js
@@ -1,19 +1,16 @@
 function initialize() {
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644)
-  };
-
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+    var mapOptions = {
+      center: new google.maps.LatLng(40.6807476,-111.8767596),
+      zoom: 11,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map-canvas"),
+        mapOptions);
 }
 
-function loadScript() {
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' +
-      'callback=initialize';
-  document.body.appendChild(script);
-}
 
-window.onload = loadScript;
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+// SLC, currently centered on Fashion Place Mall to better center on the valley
+// 40.776608,-111.920485
