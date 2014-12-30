4836c0c5fa575610f798e7c462ac359332d8a561
jmaxfield21 <Jace@Maxfields-iMac.local>
2014-12-29 20:14:31 -0700

    styling

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
 function initialize() {
     var mapOptions = {
       center: new google.maps.LatLng(40.6807476,-111.8767596),
-      zoom: 11,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+      zoom: 11
     };
     var map = new google.maps.Map(document.getElementById("map-canvas"),
         mapOptions);
