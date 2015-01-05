2fe5fbeb06fa6f2d2319852830b5d58fe4e990d6
jmaxfield21 <Jace@Maxfields-iMac.local>
2015-01-04 22:11:29 -0700

    work towards basic api implementation

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
+$(document).ready(function() {
+  
+  initialize();
+
+  $('a').on( 'click', getPlacesData );
+
+});
+
+
+var mapCenterLat = 40.6807476;
+var mapCenterLng = -111.8767596;
+
 function initialize() {
     var mapOptions = {
-      center: new google.maps.LatLng(40.6807476,-111.8767596),
+      center: new google.maps.LatLng(mapCenterLat, mapCenterLng),
       zoom: 11
     };
     var map = new google.maps.Map(document.getElementById("map-canvas"),
@@ -8,10 +20,20 @@ function initialize() {
 }
 
 
-google.maps.event.addDomListener(window, 'load', initialize);
+function getPlacesData() {
+  var type = $(this).attr('id');
 
+  var locations = $.ajax(
+      url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?parameters',
+      key: 'AIzaSyBc-VC_aNuBYz094lOMuSpt5nof1ra42bE',
+      location: ( mapCenterLat, mapCenterLng ),
+
+  );
+
+}
 
 // SLC coordinates below, 
 // 40.776608,-111.920485
 // the current settings center the map 
 // on Fashion Place Mall to better center on the valley
+// google.maps.event.addDomListener(window, 'load', initialize);
