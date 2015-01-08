90457d4c233193d199d84de094074672ab0955b4
Jace Maxfield <jace.maxfield@octanner.com>
2015-01-08 11:30:25 -0700

    maps api now operating correctly

--- js/app.js
+++ js/app.js
@@ -2,34 +2,82 @@ $(document).ready(function() {
   
   initialize();
 
-  $('a').on( 'click', getPlacesData );
+  $('a').on( 'click', function(event) {
+    event.preventDefault();
+    getPlacesData( $(this).attr('id') );
+  });
 
 });
 
+var map;
+var service;
+var infowindow;
 
-var mapCenterLat = 40.6807476;
-var mapCenterLng = -111.8767596;
+var mapCenter = new google.maps.LatLng(40.6807476, -111.8767596);
+
+var recTypes = ['aquarium', 'art_gallery', 'bowling_alley', 'casino', 'museum', 'night_club', 'park', 'stadium', 'zoo'];
+var markers = new Array();
 
 function initialize() {
     var mapOptions = {
-      center: new google.maps.LatLng(mapCenterLat, mapCenterLng),
+      center: mapCenter,
       zoom: 11
     };
-    var map = new google.maps.Map(document.getElementById("map-canvas"),
+    map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
 }
 
+function getRecTypes() {
+  return recTypes;
+}
 
-function getPlacesData() {
-  var type = $(this).attr('id');
+function getPlacesData( type ) {
+  if ( type === 'recreation' ) {
+    type = getRecTypes();
+  } else {
+    type = [type];
+  }
 
-  var locations = $.ajax(
-      url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?parameters',
-      key: 'AIzaSyBc-VC_aNuBYz094lOMuSpt5nof1ra42bE',
-      location: ( mapCenterLat, mapCenterLng ),
+  var request = {
+    location: mapCenter,
+    radius: 11000,
+    types: type
+  };
 
-  );
+  infowindow = new google.maps.InfoWindow();
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
+}
 
+function callback(results, status) {
+  if ( status == google.maps.places.PlacesServiceStatus.OK ) {
+    clearMarkers();
+    for ( var i = 0; i < results.length; i++ ) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function clearMarkers() {
+  for ( var i = 0; i < markers.length; i++ ) {
+    markers[i].setMap(null);
+  }
+  markers = new Array();
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+  
+  markers.push(marker);
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
 }
 
 // SLC coordinates below, 
